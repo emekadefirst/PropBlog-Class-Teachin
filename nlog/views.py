d6df2645b8e3a4d3a180cdde4f7338520dab9857
emekadefirst <victorchibuogwu33@gmail.com>
2025-10-01 22:25:00 +0100

    detail view

--- nlog/views.py
+++ nlog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
 def index(request):
     blogs = Blog.objects.all()
     return render(request, 'index.html', {'blogs': blogs})
+
+def detail(request, id):
+    blog = get_object_or_404(Blog, id=id)
+    return render(request, 'detail.html', {'blog': blog})
